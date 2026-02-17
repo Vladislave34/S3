eed7c228e7d4bfef3f31bbd4fa514ab0b210e76e
Ostapchuk Vlad <ostapchukvladislav77@gmail.com>
2026-02-17 16:53:03 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import boto3
+
+#print("Сало - це смачно і дуже корисно!")
+s3_client = boto3.client('s3') # S3 client
+
+
+response = s3_client.list_buckets() # List all buckets
+for bucket in response['Buckets']:
+    print(bucket)
+
+ 
